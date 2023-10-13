fe86f8c6617212353b3e264144b060fcc6d920fb
Aman404 <amanupadhyay404@gmail.com>
2023-10-13 17:56:42 +0530

    ....

--- src/rsk.presentation/Buttons/Button.test.tsx
+++ src/rsk.presentation/Buttons/Button.test.tsx
@@ -1,36 +1,28 @@
-
+import { render, fireEvent } from "@testing-library/react";
 import Button from "./Button";
-import { render } from "@testing-library/react";
 
+describe("Button Component", () => {
+    const mockOnClick = jest.fn();
+    const mockOnSubmit = jest.fn();
+    const defaultProps = {
+        name: "Test Button",
+        onClick: mockOnClick,
+        onSubmit: mockOnSubmit,
+        disabled: false,
+    };
 
-test('it renders the Button component', () => {
-
-    render(<Button name={""} onClick={""} onSubmit={""} disabled={false} />);
-});
-
-
-test('should run example code', () => {
-    // creates a new mocked function with no formal arguments.
-    expect(Button.name).toBe('Button');
-    expect(Button).toHaveLength(1);
-
-    
-
-    // // creates a deeply cloned version of the original object.
-    // expect(example.object).toEqual({
-    //     baz: 'foo',
-    //     bar: {
-    //         fiz: 1,
-    //         buzz: [],
-    //     },
-    // });
-
-    // // creates a new empty array, ignoring the original array.
-    // expect(example.array).toHaveLength(0);
-
-    // // creates a new property with the same primitive value as the original property.
-    // expect(example.number).toBe(123);
-    // expect(example.string).toBe('baz');
-    // expect(example.boolean).toBe(true);
-    // expect(example.symbol).toEqual(Symbol.for('a.b.c'));
+    test('it renders the Button component', () => {
+        render(<Button {...defaultProps}/>)
+    });
+    test("calls onClick handler when clicked", () => {
+        const { getByText } = render(<Button {...defaultProps} />);
+        const buttonElement = getByText("Test Button");
+        fireEvent.click(buttonElement);
+        expect(mockOnClick).toHaveBeenCalled();
+    });
+    test("is disabled when disabled prop is true", () => {
+        const { getByText } = render(<Button {...defaultProps} disabled={true} />);
+        const buttonElement = getByText("Test Button");
+        expect(buttonElement).toBeTruthy();
+    });
 });
